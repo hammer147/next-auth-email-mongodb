6e2fe080dd58e07d3ab51fd308dca165d2fcf175
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-06-12 13:05:14 +0200

    added page

---
+++ pages/member/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+const Member: FC = () => {
+
+  return (
+    <div>
+      <h1>Member</h1>
+      <p>
+        This page can only be accessed by members or administrators.
+      </p>
+    </div>
+  )
+}
+
+export default Member
