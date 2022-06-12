c78a7433c4c19af35ebd1bbecf24ed56fcfcc06c
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-06-12 13:23:42 +0200

    fixed ts error

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -3,12 +3,17 @@ import type { JWT } from "next-auth/jwt"
 
 import { withAuth } from "next-auth/middleware"
 
-// @ts-ignore
-const middleware: NextMiddleware = (req: NextRequest & { nextauth: { token: JWT } }) => {
-  console.log("Middleware token", req.nextauth.token)
+// // @ts-ignore
+// const middleware: NextMiddleware = (req: NextRequest & { nextauth: { token: JWT } }) => {
+//   console.log("Middleware token", req.nextauth.token)
+//   return NextResponse.next()
+// }
+
+const middleware: NextMiddleware = (req) => {
+  const token = (req as NextRequest & { nextauth: { token: JWT } }).nextauth.token
+  console.log("Middleware token", token)
   return NextResponse.next()
 }
-
 
 export default withAuth(
   middleware,
@@ -19,4 +24,5 @@ export default withAuth(
   }
 )
 
+// An example on how to wrap middleware.
 // The middleware function will only be invoked if the authorized callback returns true.
