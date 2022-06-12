5abf3f928841c8bb3c8dbb385c612976f7476940
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-06-12 09:00:34 +0200

    improved comment

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -9,4 +9,4 @@ export default withAuth({
 // With the above code, you just made sure that only user's with the admin role can 
 // access any of the pages under the /admin route. 
 // (Including nested routes as well, like /admin/settings etc.).
-// Users without the admin role are redirected to the login page.
+// Users without the admin role are redirected to the sign-in page.
