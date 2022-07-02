9be9ce4228aa2e1a59053c638ddb9bd2bb9d9d56
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-07-02 08:41:04 +0200

    upgraded middleware

--- pages/protected/_middleware.ts
+++
@@ -1,7 +0,0 @@
-export { default } from "next-auth/middleware"
-
-// With this one line, when someone tries to load any of your pages, they will have to be logged-in first. 
-// Otherwise, they are redirected to the login page.
-
-// https://next-auth.js.org/configuration/nextjs#middleware
-
